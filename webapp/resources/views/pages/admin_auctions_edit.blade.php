e94be45215b4e483431cb9de146aae13cbee6694
Joao <joaorolareis@gmail.com>
2022-11-18 10:50:25 +0000

    Changed the admin auction edit form

--- webapp/resources/views/pages/admin_auctions_edit.blade.php
+++ webapp/resources/views/pages/admin_auctions_edit.blade.php
@@ -1,22 +1,31 @@
 @extends('layouts.app')
 
+@section('styles')
+    <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
+@endsection
+
+
 @section('content')
-    <form action="/admin/{{$adminId}}/auctions/{{$auction->id}}" method="POST">
-        {{ csrf_field() }}
-        @method('PUT')
-        <div class="form-group">
-        <label for="auctionName">Auction Name</label>
-        <input name="name" id="auctionName" type="text" class="form-control"  value="{{$auction->name}}">
-        </div>
-        <div class="form-group">
-        <label for="auctionDescription">Auction Description</label>
-        <textarea  name="description" id="auctionDescription" cols="30" rows="10" >{{$auction->description}}</textarea> 
-        </div>
-        <button type="submit" class="btn btn-primary">Update</button>
-    </form>
-    <form action="/admin/{{$adminId}}/auctions/{{$auction->id}}" method="POST">
-        {{ csrf_field() }}
-        @method('DELETE')
-        <button type="submit" class="btn btn-danger">Delete</button>
-    </form>
+    <div class="container">
+        <form action="/admin/{{$adminId}}/auctions/{{$auction->id}}" method="POST">
+            {{ csrf_field() }}
+            @method('PUT')
+            <div class="form-group">
+            <label for="auctionName">Auction Name</label>
+            <input name="name" id="auctionName" type="text" class="form-control"  value="{{$auction->name}}">
+            </div>
+            <div class="form-group">
+            <label for="auctionDescription">Auction Description</label>
+            <textarea  name="description" id="auctionDescription" cols="30" rows="10" >{{$auction->description}}</textarea> 
+            </div>
+            <div class="text-center">
+                <button type="submit" class="btn btn-primary">Update</button>
+            </div>        
+        </form>
+        <form action="/admin/{{$adminId}}/auctions/{{$auction->id}}" method="POST">
+            {{ csrf_field() }}
+            @method('DELETE')
+            <button type="submit" class="btn btn-danger">Delete</button>
+        </form>
+    </div>
 @endsection
