e5848d9bce95e6e5d09daba8d9fc6a77ec3f6f9e
Afonso Baldo <up202004598@edu.fe.up.pt>
2023-01-01 19:35:55 +0000

    header responsive

--- webapp/resources/views/layouts/app.blade.php
+++ webapp/resources/views/layouts/app.blade.php
@@ -42,56 +42,84 @@
 
     <body>
 
-      <header class="d-flex flex-row justify-content-between align-items-center pb-3 mb-5">
-        <img
-          src= "{{ asset('default_images/logo_high.svg') }}"
-          id="logo"
-          height="60"
-          width="150"
-          class="m-3"
-          alt="Melody auctions Logo"
-        />
+      <header class="navbar navbar-expand-lg bg-light rounded" >
+        <div class="container-fluid navbar-container">
+        <img class="navbar-brand"
+            src= "{{ asset('default_images/logo_high.svg') }}"
+            id="logo"
+            height="60"
+            width="150"
+            class="m-3"
+            alt="Melody auctions Logo"
+          />
+          
+          <div class="fixed-options" >
 
-        <a id="home" class="nav-link h6 px-5 text-dark" href="{{ url('/') }}">Home</a>
-
-        <a id="sell" class="nav-link h6 px-5 text-dark" href="{{ url('/auction/create') }}">Sell</a>
-
-        <a id="buy" class="nav-link h6 px-5 text-dark" href="{{ url('/auction') }}">Buy</a>
-
-        <div class="d-flex flex-row flex-grow-1 m-top">
-          <div class="d-flex flex-row flex-grow-1">
-            <div class="flex-grow-1 m-auto">
-              <input id="search_bar" class="form-control border-end-0 border rounded-pill" type="search" value=@yield('searchContent')>
-              <div id="search_results" class="mx-3">
-                <div class="dropdown-menu bx-3">
-                </div>
-              </div>
-            </div>
-            <span class="input-group-append">
-                <button id="search-button" class="btn btn-outline-secondary bg-white border-bottom-0 border rounded-pill ms-n5" type="button">
-                  <i class="fa fa-search" aria-hidden="true"></i>
-                </button>
-            </span>
+            @if ($loggedIn)
+              @if(!$isAdmin)
+                  @include('partials.notifications', ['notifications' => $notifications])
+              @endif
+                  <div id= "{{ $isAdmin?'admin':'user' }}-profile" class="{{$identificator}} profile-userpic me-3">
+                    <img class="rounded-circle h6 " src="{{ asset($profilePic) }}" class="profilepic" alt="User Image">
+                  </div>
+            @else
+                  
+                <a id="login" class="button me-5" href="{{ url('/login') }}"> Log in </a>
+            @endif
+            <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          
           </div>
-          <div id="auctionsOrUsers" class="dropdown ms-3 me-5">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="auctionsOrUsersButton" data-bs-toggle="dropdown" aria-expanded="false">Auctions</button>
-            <ul class="dropdown-menu" aria-labelledby="auctionsOrUsersButton">
-              <li><p class="dropdown-item chosen">Auctions</p></li>
-              <li><p class="dropdown-item">Users</p></li>
+
+          <div class="navbar-collapse collapse" id="navbar" >
+            <ul class="navbar-nav justify-content-between align-items-center w-100 mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a id="home" class="nav-link h6 px-5 text-dark" href="{{ url('/') }}">Home</a>
+              </li>
+              <li class="nav-item">
+                <a id="sell" class="nav-link h6 px-5 text-dark" href="{{ url('/auction/create') }}">Sell</a>
+              </li>
+              <li class="nav-item">
+                <a id="buy" class="nav-link h6 px-5 text-dark" href="{{ url('/auction') }}">Buy</a>
+              </li>
+
+              <li class="nav-item w-100 flex-grow-1">
+                <div class="d-flex flex-row flex-grow-1 m-top">
+                  <div class="d-flex flex-row flex-grow-1">
+                    <div class="flex-grow-1 m-auto">
+                      <input id="search_bar" class="form-control border-end-0 border rounded-pill" type="search" value=@yield('searchContent')>
+                      <div id="search_results" class="mx-3">
+                        <div class="dropdown-menu bx-3">
+                        </div>
+                      </div>
+                    </div>
+                    <span class="input-group-append">
+                        <button id="search-button" class="btn btn-outline-secondary bg-white border-bottom-0 border rounded-pill ms-n5" type="button">
+                          <i class="fa fa-search" aria-hidden="true"></i>
+                        </button>
+                    </span>
+                  </div>
+                  <div id="auctionsOrUsers" class="dropdown ms-3 me-5">
+                    <button class="btn dropdown-toggle" type="button" id="auctionsOrUsersButton" data-bs-toggle="dropdown" aria-expanded="false">Auctions</button>
+                    <ul class="dropdown-menu" aria-labelledby="auctionsOrUsersButton">
+                      <li><p class="dropdown-item chosen">Auctions</p></li>
+                      <li><p class="dropdown-item">Users</p></li>
+                    </ul>
+                  </div>
+                </div>
+              </li>
+
+              
             </ul>
           </div>
+
+          
+
         </div>
-        @if ($loggedIn)
-          @if(!$isAdmin)
-            @include('partials.notifications', ['notifications' => $notifications])
-          @endif
-          <div id= "{{ $isAdmin?'admin':'user' }}-profile" class="{{$identificator}} profile-userpic me-3">
-            <img class="rounded-circle" src="{{ asset($profilePic) }}" class="profilepic" alt="User Image">
-          </div>
-        @else
-          <a id="login" class="button me-5" href="{{ url('/login') }}"> Log in </a>
-        @endif
+        
       </header>
+
       <main>
         <section id="content">
           @yield('content')
