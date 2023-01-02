92ec9d14d8418030b31c280f008a27dbb17f5af1
Afonso Baldo <up202004598@edu.fe.up.pt>
2023-01-02 19:10:58 +0000

    fix script

--- webapp/docker_run.sh
+++ webapp/docker_run.sh
@@ -1,27 +1,16 @@
 #!/bin/bash
 set -e
 
-cd /var/www
+cd /var/www;
 env >> /var/www/.env
+
+php artisan config:cache
 php artisan clear-compiled
 php artisan config:clear
 php artisan cache:clear
 php artisan storage:link
+php artisan schedule:work &
+
 php-fpm8.1 -D
-
-
-# Add cron job into cronfile
-* * * * * cd /var/www && php artisan schedule:run >> /dev/null 2>&1
-
-# Install cron job
-crontab cronfile
-
-# Remove temporary file
-rm cronfile
-
-env >> /var/www/.env
-php-fpm8.0 -D
-# Start cron
-cron
 
 nginx -g "daemon off;"
